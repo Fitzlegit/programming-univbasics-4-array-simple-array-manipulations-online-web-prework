0f5acb57c2a6cc2528c038494cbeb6b136cd2ef8
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-17 14:54:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,8 +32,8 @@ def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
 end
 
-def using_uniq(haircuts, style)
-  haircuts.delete(style)
+def using_uniq(haircuts)
+  haircuts.delete()
 end
   
   
