0c68b226d39381416d937831201951066665b715
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-17 14:18:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,6 +16,6 @@ def pop_with_args(dog_breeds)
   dog_breeds.pop(2)
 end
 
-def shift_with_args(ice_cream_brands)
-  ice_cream_brands.shift
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
 end
