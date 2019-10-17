2da7aac093352020d30ca1a04edca00433705c1a
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-17 14:23:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,5 +29,5 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
-  list_of_programming_languages.using_insert[3] = another_language
+  list_of_programming_languages.insert[3] = another_language
 end
